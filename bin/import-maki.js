d3121affaa18b2c6bf0668cea5f5ecce2abaef64
yetzt <git@yetzt.me>
2023-03-13 16:44:39 +0100

    derive icon names for shortbread scheme

--- bin/import-maki.js
+++ bin/import-maki.js
@@ -8,6 +8,56 @@ const fs = require("fs");
 
 const dest = path.resolve(__dirname,path.join("..","iconsets","icon"));
 
+const maki2shortbread = {
+	"alcohol-shop": "alcohol",
+	"amusement-park": "theme_park",
+	"art-gallery": "arts_centre",
+	"bar": "nightclub",
+	"beer": "pub",
+	"bicycle-share": "bicycle_rental",
+	"camp-site": ["camp_site", "caravan_site"],
+	"car-rental": ["car_rental","car_sharing"],
+	"cemetery": "grave_yard",
+	"college": ["college","university"],
+	"doctor": "doctors",
+	"dog-park": "dog_park",
+	"drinking-water": ["drinking_water","water_well"],
+	"embassy": ["diplomatic", "embassy"],
+	"emergency-phone": "phone",
+	"fast-food": "fast_food",
+	"fire-station": "fire_station",
+	"fitness-centre": "sports_centre",
+	"garden-centre": "garden_centre",
+	"golf": "golf_course",
+	"greengrocer": "grocery",
+	"historic": ["archaelogical_site","battlefield","fort","memorial","ruins"],
+	"hospital": ["clinic","hospital"],
+	"lodging": [ "bed_and_breakfast", "guest_house", "hostel", "hotel", "motel" ],
+	"mobile-phone": "mobile_phone",
+	"observation-tower": "tower",
+	"picnic-site": "picnic_site",
+	"place-of-worship": "place_of_worship",
+	"post": "post_office",
+	"religious-christian": "wayside_cross",
+	"restaurant": ["food_court","restaurant"],
+	"shop": [ "department_store", "general", "mall", "supermarket" ],
+	"swimming": "swimming_pool",
+	"toilet": "toilets",
+	"town-hall": ["public_building","townhall"],
+	"waste-basket": "waste_basket",
+};
+
+const extra2shortbread = {
+	"atm": "atm",
+	"beergarden": "biergarten",
+	"bench": "bench",
+	"car-wash": "car_wash",
+	"chalet": ["chalet","alpine_hut"],
+	"community": "community_centre",
+	"fountain": "fountain",
+	"justice": "courthouse",
+};
+
 // simple https client with redirects
 function get(url, fn){
 	https.get(url, function(res){
@@ -52,15 +102,55 @@ fs.mkdir(dest, { recursive: true }, function(){
 					return '<svg'+before+'height="'+(parseInt(h,10)*12)+'"'+after+'>';
 				}));
 
-				// save
-				fs.writeFile(path.join(dest,filename), svg, function(err){
-					if (!err) console.log("Imported: %s", filename);
+				const id = path.basename(filename, ".svg");
+
+				let dests = [ path.join(dest,filename) ];
+				if (maki2shortbread.hasOwnProperty(id)) {
+					if (Array.isArray(maki2shortbread[id])) {
+						maki2shortbread[id].forEach(function(destname){
+							dests.push(path.join(dest,destname+".svg"));
+						});
+					} else {
+						dests.push(path.join(dest,maki2shortbread[id]+".svg"));
+					}
+				}
+
+				Promise.allSettled(dests.map(function(d){
+					return new Promise(function(resolve,reject){
+						fs.writeFile(d, svg, function(err){
+							if (!err) console.log("Imported: %s → %s", filename, path.basename(d));
+							resolve();
+						});
+					});
+				})).then(function(){
 					next();
 				});
+
 			});
 
-		}).on("end", function(){
-			console.log("Done.");
+		}).on("close", function(){
+
+			// extra icons
+			Promise.allSettled(Object.entries(extra2shortbread).reduce(function(jobs,[src,dests]){
+				if (!Array.isArray(dests)) dests = [ dests ];
+
+				dests.forEach(function(d){
+					jobs.push(new Promise(function(resolve, reject){
+						fs.copyFile(
+							path.resolve(dest,"../extra",src+".svg"),
+							path.resolve(dest,d+".svg"),
+							function(err){
+								if (!err) console.log("Extra: %s.svg → %s.svg", src, d);
+								resolve();
+							});
+					}));
+				});
+
+				return jobs;
+
+			},[])).then(function(){
+				console.log("Done.");
+			});
 		}));
 	});
 });
